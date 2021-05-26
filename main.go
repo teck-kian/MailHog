0b45515564d52ac509fdf519d15d8bb631f51679
teck-kian <tkian92@gmail.com>
2021-05-26 10:44:08 +0800

    Update import to forked server code

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
-	"github.com/mailhog/MailHog-Server/api"
+	"github.com/teck-kian/MailHog-Server/api"
 	cfgapi "github.com/mailhog/MailHog-Server/config"
 	"github.com/mailhog/MailHog-Server/smtp"
 	"github.com/mailhog/MailHog-UI/assets"
